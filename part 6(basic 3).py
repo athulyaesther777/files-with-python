2dfd388267eebe1bdde785ef9b0921f3423ffa09
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-02 15:04:32 +0530

    Add files via upload

---
+++ part 6(basic 3).py
@@ -0,0 +1,16 @@
+import pyautogui
+
+# to perform different types of mouse clicks
+# at specific coordinates on the screen
+
+# Single click at the absolute position (100, 100) on the screen
+pyautogui.click(100, 100)
+
+# Double click at the absolute position (100, 100) on the screen
+pyautogui.doubleClick(100, 100)
+
+# Right click at the absolute position (100, 100) on the screen
+pyautogui.rightClick(100, 100)
+
+# Middle click
+pyautogui.middleClick(100, 100)
