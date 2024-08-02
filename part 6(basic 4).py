706f9b6c44183c1cab97e1ced3470901ee61441f
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-02 15:09:43 +0530

    Add files via upload

---
+++ part 6(basic 4).py
@@ -0,0 +1,7 @@
+import pyautogui
+
+pyautogui.dragTo(200, 200, duration=2)  # Drag to (200, 200) over 2 seconds
+pyautogui.dragRel(50, 50, duration=1)   # Drag 50 pixels right and 50 pixels down
+
+#just run this codes to get to know how it works , it similar to
+# the movements that we already discussed
