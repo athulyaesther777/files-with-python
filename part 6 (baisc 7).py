2df1eb9f0960f09170df81056adfa0ac3dcc2104
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-02 15:47:49 +0530

    Add files via upload

---
+++ part 6 (baisc 7).py
@@ -0,0 +1,10 @@
+
+import pyautogui
+
+# Simulate pressing the "Home" key
+pyautogui.press("home")
+
+"""
+Effect: The "Home" key usually moves the cursor to the beginning of the current line or the start of a document, 
+depending on the application and context.
+"""
